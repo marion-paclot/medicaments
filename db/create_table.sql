4797d3312ecf92745bc834b73f6e2899ae17aafb
Marion Paclot <marion.paclot@gmail.com>
2020-02-25 18:37:53 +0100

    Add files via upload

---
+++ db/create_table.sql
@@ -0,0 +1,55 @@
+-- Database: Medicaments
+-- Création des tables
+DROP TABLE IF EXISTS consommation ;
+DROP TABLE IF EXISTS referentiel;
+
+CREATE TABLE public.referentiel
+(	cip13 CHARACTER(13),
+	cis CHARACTER(8), 
+	cip7 CHARACTER(7), 
+	libelle CHARACTER VARYING,
+	statut CHARACTER VARYING, 
+	typeaction CHARACTER VARYING, 
+	dateaction DATE, 
+	agrement CHARACTER VARYING,
+	taux INTEGER,
+	prixhorshono DECIMAL(10,2),
+	prixtotal DECIMAL(10,2),
+	honoraires DECIMAL(10,2),
+	infos CHARACTER VARYING,
+	denomination CHARACTER VARYING,
+	forme CHARACTER VARYING,
+	voieadmin CHARACTER VARYING,
+	statutamm CHARACTER VARYING,
+	typeprocamm CHARACTER VARYING,
+	etatcommercialisation CHARACTER VARYING,
+	dateamm DATE,
+	statutbdm CHARACTER VARYING,
+	numammce CHARACTER VARYING,
+	titulaire CHARACTER VARYING,
+	surveillance CHARACTER VARYING,
+	nomcis CHARACTER VARYING, 
+	numfamille CHARACTER VARYING,
+	nomfamille CHARACTER VARYING,
+	typemed INTEGER, 
+	tri INTEGER,
+	nomcip CHARACTER VARYING,
+	nbunite INTEGER,
+	numfamilledci CHARACTER VARYING
+
+)
+WITH (
+    OIDS = FALSE
+) ;
+
+CREATE TABLE public.consommation
+(	cip13 CHARACTER(13),
+ 	lieu CHARACTER VARYING,
+ 	mois DATE,
+ 	nb DECIMAL(10,2),
+ 	mt DECIMAL(10,2),
+ 	base DECIMAL(10,2)
+)
+WITH (
+    OIDS = FALSE
+) ;
---
+++ db/gestion_bd.sh
@@ -0,0 +1,15 @@
+# Se mettre dans le dossier medicament
+cd /
+
+#########################################################
+# Création de la base de données -- à faire une fois
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS medicaments;"
+sudo -u postgres psql -c "CREATE DATABASE medicaments WITH ENCODING = 'UTF8';"
+sudo -u postgres psql -c "ALTER DATABASE medicaments SET datestyle TO ""ISO, DMY"";"
+sudo -u postgres psql -d medicaments -f "db/create_table.sql"
+
+# Chargement des données 
+sudo -u postgres psql medicaments -c "COPY consommation FROM '/srv/shiny-server/medicaments/data/importbdd/consommation.csv' 
+			WITH (FORMAT CSV, DELIMITER ',', NULL 'NA', HEADER, encoding 'UTF8', ESCAPE '''') ;"
+sudo -u postgres psql medicaments -c "COPY referentiel FROM '/srv/shiny-server/medicaments/data/importbdd/medicaments.csv' 
+			WITH (FORMAT CSV, DELIMITER ',', NULL 'NA', HEADER, encoding 'UTF8', ESCAPE '''') ;"
