a804bb69136c59f63c5484983d5064715e7abcf3
Marion Paclot <marion.paclot@gmail.com>
2020-12-04 14:50:56 +0100

    Add files via upload

--- db/create_table.sql
+++ db/create_table.sql
@@ -56,3 +56,8 @@ CREATE TABLE public.consommation
 WITH (
     OIDS = FALSE
 ) ;
+
+
+-- "C:\\Program Files (x86)\\pgAdmin 4\\v4\\runtime\\psql.exe" --command " "\\copy public.consommation (cip13, lieu, mois, nb, mt, base) FROM 'C:/Users/Nous/Desktop/Marion/Etalab/Sante/app/data/IMPORT~1/CONSOM~1.CSV' DELIMITER ',' CSV HEADER QUOTE '\"' NULL 'NA' ESCAPE '''';""
+
+-- "C:\\Program Files (x86)\\pgAdmin 4\\v4\\runtime\\psql.exe" --command " "\\copy public.referentiel (cip13, cis, cip7, libelle, statut, typeaction, dateaction, agrement, taux, prixhorshono, prixtotal, honoraires, infos, denomination, forme, voieadmin, statutamm, typeprocamm, etatcommercialisation, dateamm, statutbdm, numammce, titulaire, surveillance, nomcis, numfamille, nomfamille, typemed, tri, condition, nomcip, atc, nbunite, numfamilledci, acces_direct, mitm) FROM 'C:/Users/Nous/Desktop/Marion/Etalab/Sante/app/data/IMPORT~1/REFERE~1.CSV' DELIMITER ',' CSV HEADER QUOTE '\"' NULL 'NA' ESCAPE '''';""
